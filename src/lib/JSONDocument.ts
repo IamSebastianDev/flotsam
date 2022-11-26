9fc6a8f0094cb12c98442cba1f6dcf37a22aff52
Sebastian <sebastian@iamsebastian.dev>
2022-11-26 22:38:47 +0100

    docs: update documentation

--- src/lib/JSONDocument.ts
+++ src/lib/JSONDocument.ts
@@ -12,9 +12,29 @@ export class JSONDocument<T extends Record<string, unknown>> {
         return this.#id;
     }
 
-    toFile() {
+    /**
+     * @public
+     * @method
+     * @description
+     * Method to convert the internal data of the `JSONDocument` into a JSON
+     * encoded string to store inside a serialized file.
+     *
+     * @returns { string } the JSON encoded string to store as file
+     */
+
+    toFile(): string {
         return JSON.stringify({ _id: this.#id.str, _: this.data._ });
     }
+
+    /**
+     * @public
+     * @method
+     * @description
+     * Method to convert the internal data of the `JSONDocument` into a JS Object containing
+     * data and the exposed id
+     *
+     * @returns { string } the JSON encoded string to store as file
+     */
 
     toDoc(): Document<T> {
         return { ...this.data._, _id: this.#id };
--- src/types/Document.d.ts
+++ src/types/Document.d.ts
@@ -2,4 +2,10 @@
 
 import { ObjectId } from '../lib';
 
+/**
+ * @type { Document }
+ * @description
+ * A Document is the combination of it's generic Data `T` and the associated `ObjectId`
+ */
+
 export type Document<T> = T & { _id: ObjectId };
