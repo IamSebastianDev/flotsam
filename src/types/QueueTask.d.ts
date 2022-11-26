44e2a4f512ca54621e7b2b948fcb7377c02bf167
Sebastian <sebastian@iamsebastian.dev>
2022-11-26 22:39:04 +0100

    docs: update documentation

--- src/types/QueueTask.d.ts
+++ src/types/QueueTask.d.ts
@@ -1,5 +1,11 @@
 /** @format */
 
+/**
+ * @description
+ * Object used as a task inside the queue, containing methods
+ * to resolve, reject and execute the task
+ */
+
 export type QueueTask = {
     execute: () => Promise<any>;
     resolve: (value: any) => any;
