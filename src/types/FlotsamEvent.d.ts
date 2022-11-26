27bc92c45167fbab7a1eaeb2d3b89322eec1cc00
Sebastian <sebastian@iamsebastian.dev>
2022-11-26 22:38:54 +0100

    docs: update documentation

--- src/types/FlotsamEvent.d.ts
+++ src/types/FlotsamEvent.d.ts
@@ -2,6 +2,17 @@
 
 /**
  * Union type representing the different events that are dispatched by Flotsam during operation.
+ *
+ * - 'close': emitted when the connection to the database is closed.
+ * - 'connect': emitted when the connection to the database is first created.
+ * - 'insert':  emitted when a document is inserted into the database.
+ * - 'upsert':
+ * - 'delete': emitted when a document is deleted from the database.
+ * - 'update': emitted when a document is updated.
+ * - 'serialize': emitted when a collection is serialized.
+ * - 'deserialize': emitted when a collection is deserialized.
+ * - 'drop': emitted when a collection is dropped.
+ * - 'error': emitted when a error occurs.
  */
 
 export type FlotsamEvent =
