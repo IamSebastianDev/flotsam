8509254f99dfca299051cd5aef5171ed2393abe0
Sebastian <sebastian@iamsebastian.dev>
2022-11-27 09:36:59 +0100

    feat: create simplified find by property options

--- src/lib/Evaluators/evaluateFindOptions.ts
+++ src/lib/Evaluators/evaluateFindOptions.ts
@@ -25,5 +25,21 @@ export const evaluateFindOptions = <T extends Record<string, unknown>>(
     });
 };
 
-    return Object.entries(findOptions).every(([prop, evaluator]) => evaluator(document[prop], prop));
+/**
+ * @description
+ * Function to evaluate a `Document` using a given set of simplified find by property options.
+ *
+ * @param { Document } document - the `Document`whose properties to check
+ * @param { FindOByProperty } findOptions - the passed find options to evaluate
+ * @returns { boolean } a boolean indicating if the `Document` satisfies the
+ * conditions in the find options
+ */
+
+export const evaluateFindByPropertyOptions = <T extends Record<string, unknown>>(
+    document: Document<T>,
+    findOptions: FindByProperty<T>
+): boolean => {
+    return Object.entries(findOptions).every(([prop, evaluator]) => {
+        return typeof evaluator === 'function' ? evaluator(document[prop], prop) : document[prop] === evaluator;
+    });
 };
--- src/lib/Evaluators/index.ts
+++ src/lib/Evaluators/index.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-export { evaluateFindOptions } from './evaluateFindOptions';
+export { evaluateFindOptions, evaluateFindByPropertyOptions } from './evaluateFindOptions';
 export { Exactly } from './Exactly.evaluator';
 export { Is } from './Is.evaluator';
 export { Like } from './Like.evaluator';
