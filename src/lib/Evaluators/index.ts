2c0147fd28b2418581228743a81e6a03398538ce
Sebastian <sebastian@iamsebastian.dev>
2022-11-30 23:07:38 +0100

    refactor: remove unnecessary function

--- src/lib/Evaluators/index.ts
+++ src/lib/Evaluators/index.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-export { evaluateFindOptions, evaluateFindByPropertyOptions } from './evaluateFindOptions';
+export { evaluateFindOptions } from './evaluateFindOptions';
 export { Exactly } from './Exactly.evaluator';
 export { Is } from './Is.evaluator';
 export { Like } from './Like.evaluator';
