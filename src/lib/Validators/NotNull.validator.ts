3f678ec5b7b7ca76069fd78d1a391b69954eb3c5
Sebastian <sebastian@iamsebastian.dev>
2022-12-06 01:32:30 +0100

    fix: check explicit for undefined values

--- src/lib/Validators/NotNull.validator.ts
+++ src/lib/Validators/NotNull.validator.ts
@@ -3,7 +3,7 @@
 import { FlotsamValidationError } from '../../utils';
 
 export const NotNull = (value: unknown, propertyName: string) => {
-    if (value !== null) return true;
+    if (value !== null && value !== undefined) return true;
 
     throw new FlotsamValidationError(`Property '${propertyName}' can not be NULL.`);
 };
