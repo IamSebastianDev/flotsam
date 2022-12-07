ccebb668a7952fc7f9218528cdf671776f6d0540
Sebastian Heinz <sebastian.heinz@adabay.rocks>
2022-12-07 12:37:38 +0100

    docs: update documentation

--- src/lib/Validators/NotNull.validator.ts
+++ src/lib/Validators/NotNull.validator.ts
@@ -3,7 +3,33 @@
 import { ValidatorFunction } from '../../types';
 import { FlotsamValidationError } from '../../utils';
 
-export const NotNull: ValidatorFunction = (value: unknown, propertyName: string) => {
+/**
+ * @throws
+ * @description
+ * Evaluator Function to validate a Value to be not Null or Undefined.
+ *
+ * -----
+ *
+ *@example
+ * ```ts
+ * import { Flotsam } from "flotsam/db";
+ * import { NotNull, IsString } from "flotsam/validators";
+ *
+ * const collection = await db.collect<{ name: string }>('collection', {
+ *      validate: {
+ *          name: [NotNull, IsType('string')]
+ *      }
+ * });
+ *
+ * ```
+ * ---
+ *
+ * @param { unknown } value - the value to validate
+ * @param { string } propertyName - the name of the property that is validated
+ * @returns { boolean } true if the property was successfully validated
+ */
+
+export const NotNull: ValidatorFunction = (value: unknown, propertyName: string): boolean => {
     if (value !== null && value !== undefined) return true;
 
     throw new FlotsamValidationError(`Property '${propertyName}' can not be NULL.`);
