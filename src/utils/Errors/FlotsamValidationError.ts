4b1f19fafe697127581bb2ceb0b7cf75f2a39a54
Sebastian <sebastian@iamsebastian.dev>
2022-12-08 22:19:27 +0100

    fix: remove unnecessary property

--- src/utils/Errors/FlotsamValidationError.ts
+++ src/utils/Errors/FlotsamValidationError.ts
@@ -4,7 +4,6 @@ import { FlotsamError } from './FlotsamError';
 
 export class FlotsamValidationError extends FlotsamError {
     public name: string = 'FlotsamValidationError';
-    public reported: boolean = false;
 
     constructor(message: string) {
         super(`\x1b[31m[Error][Validator]\x1b[0m ${message}`);
