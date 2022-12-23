cb0977f3eb29c64297196029551b5589ebc4abc8
Sebastian <sebastian@iamsebastian.dev>
2022-12-23 15:37:11 +0100

    test: improve flotsam instance test

--- tests/Flotsam.spec.ts
+++ tests/Flotsam.spec.ts
@@ -4,5 +4,7 @@ import test from 'ava';
 import { Flotsam } from '../src';
 
 test('[Flotsam] Instances correctly', (t) => {
-    t.is(typeof new Flotsam({ root: './store' }), 'object');
+    const db = new Flotsam({ root: './store' });
+    t.is(typeof db, 'object');
+    t.assert(db instanceof Flotsam);
 });
