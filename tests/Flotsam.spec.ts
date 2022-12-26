79f01db45824b0a1048872ab6f443fae44462b2b
Sebastian <sebastian@iamsebastian.dev>
2022-12-26 22:50:22 +0100

    test: improve test coverage for collection

--- tests/Flotsam.spec.ts
+++ tests/Flotsam.spec.ts
@@ -96,7 +96,7 @@ test.serial('[Flotsam] Correctly deserializes an encrypted collection', async (t
 });
 
 test.serial('[Flotsam] Inserts a record', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -109,7 +109,7 @@ test.serial('[Flotsam] Inserts a record', async (t) => {
 });
 
 test.serial('[Flotsam] Inserts a record and validates them', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam', {
@@ -126,7 +126,7 @@ test.serial('[Flotsam] Inserts a record and validates them', async (t) => {
 });
 
 test.serial('[Flotsam] Inserts multiple records', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -139,7 +139,7 @@ test.serial('[Flotsam] Inserts multiple records', async (t) => {
 });
 
 test.serial('[Flotsam] Finds a record using find options', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -158,7 +158,7 @@ test.serial('[Flotsam] Finds a record using find options', async (t) => {
 });
 
 test.serial('[Flotsam] Finds multiple records using find options', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -179,7 +179,7 @@ test.serial('[Flotsam] Finds multiple records using find options', async (t) => 
 });
 
 test.serial('[Flotsam] Finds multiple records using find options and sort them descending', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ index: number }>('flotsam');
@@ -204,7 +204,7 @@ test.serial('[Flotsam] Finds multiple records using find options and sort them d
 });
 
 test.serial('[Flotsam] Finds multiple records using find options and sort them ascending', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ index: number }>('flotsam');
@@ -228,8 +228,62 @@ test.serial('[Flotsam] Finds multiple records using find options and sort them a
     t.is(items[1].index, 1);
 });
 
+test.serial('[Flotsam] Finds multiple records using find options and limit them', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ index: number }>('flotsam');
+
+    await Promise.all(
+        Array(20)
+            .fill(undefined)
+            .map(async (_, i) => await collection.insertOne({ index: i }))
+    );
+
+    const items = await collection.findMany({
+        where: {},
+        limit: 5,
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(items);
+    t.is(items.length, 5);
+});
+
+test.serial('[Flotsam] Finds multiple records using find options and paginate them', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ index: number }>('flotsam');
+
+    await Promise.all(
+        Array(20)
+            .fill(undefined)
+            .map(async (_, i) => await collection.insertOne({ index: i }))
+    );
+
+    const items = await collection.findMany({
+        where: {},
+        order: {
+            by: 'ASC',
+            property: 'index',
+        },
+        skip: 10,
+        take: 10,
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(items);
+    t.is(items.length, 10);
+    t.is(items[0].index, 10);
+});
+
 test.serial('[Flotsam] Finds multiple records using findByProperty options', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -248,7 +302,7 @@ test.serial('[Flotsam] Finds multiple records using findByProperty options', asy
 });
 
 test.serial('[Flotsam] Finds a record using findByProperty options', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -265,7 +319,7 @@ test.serial('[Flotsam] Finds a record using findByProperty options', async (t) =
 });
 
 test.serial('[Flotsam] Finds a record using findById', async (t) => {
-    const db = new Flotsam({ root: './tests/.store', quiet: true, auth: 'auth' });
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
 
     await db.connect();
     const collection = await db.collect<{ test: string }>('flotsam');
@@ -283,4 +337,246 @@ test.serial('[Flotsam] Finds a record using findById', async (t) => {
 
     t.truthy(item);
     t.is(item?.test, 'test');
+});
+
+test.serial('[Flotsam] Updates a record using find options', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    await collection.insertOne({ test: 'test' });
+
+    const updated = await collection.updateOne(
+        {
+            where: {
+                test: 'test',
+            },
+        },
+        { test: 'test2' }
+    );
+
+    if (!updated) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    const item = await collection.findOneById(updated.id);
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(item);
+    t.is(item?.test, 'test2');
+});
+
+test.serial('[Flotsam] Updates a record by property search', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    await collection.insertOne({ test: 'test' });
+
+    const updated = await collection.updateOneBy(
+        {
+            test: 'test',
+        },
+        { test: 'test2' }
+    );
+
+    if (!updated) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    const item = await collection.findOneById(updated.id);
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(item);
+    t.is(item?.test, 'test2');
+});
+
+test.serial('[Flotsam] Updates a record by Id', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    const inserted = await collection.insertOne({ test: 'test' });
+
+    if (!inserted) {
+        return t.fail('Document incorrectly inserted.');
+    }
+
+    const updated = await collection.updateOneById(inserted.id, { test: 'test2' });
+
+    if (!updated) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    const item = await collection.findOneById(updated.id);
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(item);
+    t.is(item?.test, 'test2');
+});
+
+test.serial('[Flotsam] Update multiple records using find options', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ index: number }>('flotsam');
+
+    await Promise.all(
+        Array(20)
+            .fill(undefined)
+            .map(async (_, i) => await collection.insertOne({ index: i }))
+    );
+
+    const updated = await collection.updateMany(
+        {
+            where: {},
+        },
+        {
+            index: 20,
+        }
+    );
+
+    if (!updated) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    const items = await collection.findMany({
+        where: {},
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(items);
+    t.is(items.length, 20);
+    t.assert(items.every(({ index }) => index === 20));
+});
+
+test.serial('[Flotsam] Deletes a record using find options', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    const inserted = await collection.insertOne({ test: 'test' });
+
+    if (!inserted) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    await collection.deleteOne({
+        where: {
+            test: 'test',
+        },
+    });
+
+    const item = await collection.findOne({
+        where: {
+            test: 'test',
+        },
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.is(item, null);
+});
+
+test.serial('[Flotsam] Deletes a record by a property', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    const inserted = await collection.insertOne({ test: 'test' });
+
+    if (!inserted) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    await collection.deleteOneBy({
+        test: 'test',
+    });
+
+    const item = await collection.findOne({
+        where: {
+            test: 'test',
+        },
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.is(item, null);
+});
+
+test.serial('[Flotsam] Deletes a record using its id', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ test: string }>('flotsam');
+    const inserted = await collection.insertOne({ test: 'test' });
+
+    if (!inserted) {
+        return t.fail('Document incorrectly updated.');
+    }
+
+    await collection.deleteOneById(inserted.id);
+    const item = await collection.findOneById(inserted.id);
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.is(item, null);
+});
+
+test.serial('[Flotsam] Delete multiple records using find options', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ index: number }>('flotsam');
+
+    await Promise.all(
+        Array(20)
+            .fill(undefined)
+            .map(async (_, i) => await collection.insertOne({ index: i }))
+    );
+
+    await collection.deleteMany({
+        where: {},
+    });
+
+    const items = await collection.findMany({
+        where: {},
+    });
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.truthy(items);
+    t.is(items.length, 0);
+});
+
+test.serial('[Flotsam] Should give correct number of stored documents', async (t) => {
+    const db = new Flotsam({ root: './tests/.store', quiet: true });
+
+    await db.connect();
+    const collection = await db.collect<{ index: number }>('flotsam');
+
+    await Promise.all(
+        Array(20)
+            .fill(undefined)
+            .map(async (_, i) => await collection.insertOne({ index: i }))
+    );
+
+    const count = await collection.count;
+
+    await db.jettison('flotsam');
+    await db.close();
+
+    t.is(count, 20);
 });
