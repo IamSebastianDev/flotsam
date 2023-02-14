e2550c3506664c6a2f70fbb14651b3e21bc11ee2
Sebastian <sebastian@iamsebastian.dev>
2023-02-14 01:02:03 +0100

    docs: improve documentation

--- src/types/Subscriber.d.ts
+++ src/types/Subscriber.d.ts
@@ -1,6 +1,27 @@
 /** @format */
 
+/**
+ * @description
+ * A Subscriber returned by the `subscribe()` method of an Observable.
+ * The Subscriber contains two methods to control the Subscription.
+ * `dispose()` will remove the Subscriber from the Observable and end the
+ * subscription.
+ * `once` will dispose the Subscriber after the next emit of the Observable.
+ */
+
 export type Subscriber<T> = {
+    /**
+     * @description
+     * Method to dispose the created Subscriber.
+     */
+
     dispose: () => void;
+
+    /**
+     * @description
+     * Method to dispose the Subscriber after the next emit
+     * of the Source Observable.
+     */
+
     once: () => void;
 };
--- src/types/Subscription.d.ts
+++ src/types/Subscription.d.ts
@@ -1,3 +1,10 @@
 /** @format */
 
+/**
+ * @description
+ * Function passed to the `subscribe()` method of a Observable to call
+ * whenever the Observable emits. Accepts the value and previous value of
+ * the Observable.
+ */
+
 export type Subscription<T> = (value: T, previousValue?: T) => void;
